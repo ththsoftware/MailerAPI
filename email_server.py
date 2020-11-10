3065e165133e95153caa59e9eeb1bd345fe23bf7
ththsoftware <thomas@ththsoftware.com>
2020-11-10 06:03:38 -0700

    Added functionality to test against input for JSON data, although this could be skipped I feel as though the verbose nature of this will help in troubleshooting.

--- email_server.py
+++ email_server.py
@@ -1,5 +1,5 @@
 from mail.mailer import Mailer
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__)
 
@@ -7,10 +7,13 @@ app = Flask(__name__)
 @app.route("/mail", methods=['POST', 'GET'])
 def mail():
     if request.method == "POST":
-        data = request.get_json()
-        mailer = Mailer(data['name'], data['email'], data['subject'], data['message'])
-        response = mailer.send()
-        return make_response(response)
+        if request.is_json():
+            data = request.get_json()
+            mailer = Mailer(data['name'], data['email'], data['subject'], data['message'])
+            response = mailer.send()
+            return make_response(jsonify(response))
+        else:
+            return make_response(jsonify('No input detected.'))
     elif request.method == "GET":
         return 'Thank you for your interest in the site! Mailer is online.'
 
